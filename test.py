a37a679d8c0b7dd30628b87820e460f695ac7f60
nguerre <smnguerre@gmail.com>
2025-01-16 15:23:55 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+age=int(input("entrer l'age: "))
+if age==6 or age==7:
+    print("POUSSIN")
+elif age==8 or age==9:
+    print("PUPILLE")
+elif age==10 or age==11:
+    print("MINIME")
+elif age>12:
+    print("CADET")
